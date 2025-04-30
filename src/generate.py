806722fc4a01e2569972419ca81c9c352baad84d
abdqudus <abdkhudusadeberyo@gmail.com>
2025-04-30 16:53:46 +0100

    Fixed Github page issue

--- src/generate.py
+++ src/generate.py
@@ -22,12 +22,12 @@ def generate_page(from_path, template_path, dest_path,basepath):
     
     html_string = markdown_to_html_node(from_content).to_html()
     title = extract_title(from_content)
-    html_string = html_string.replace('src="/', f'src="{basepath}')
     template_content = template_content.replace('{{ Title }}',title[0])
-    template_content = template_content.replace('href="/',f'href="{basepath}')
     template_content = template_content.replace('{{ Content }}',html_string)
+    template_content = template_content.replace('href="/',f'href="{basepath}')
+    html_string = html_string.replace('src="/', f'src="{basepath}')
    
-
+    
     with open(dest_path, 'w', encoding='utf-8') as file:
         file.write(template_content)
 
